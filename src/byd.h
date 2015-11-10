07fc48b56ab6a5bb8b5388a98523ff22c3c1f44a
Kyle Rankin <github@greenfly.net>
2015-11-10 06:57:18 -0800

    Enable byd object no matter the kernel config
    It turns out that the BYD code was only being included based on a kernel
    config option that the standard Debian kernel was not going to set. I
    needed to modify the Makefile to force the inclusion of the module
    (after all, if you are building this it's because you want it enabled).
    
    I also had to remove ifdef code that referenced the config option from
    byd.h, otherwise I would get complaints about structs being initialized
    twice. The code in its current state builds and loads appropriately
    against a stock Debian stretch kernel

--- src/byd.h
+++ src/byd.h
@@ -12,20 +12,7 @@
 #ifndef __BYD_H
 #define __BYD_H
 
-#ifdef CONFIG_MOUSE_PS2_BYD
 int byd_detect(struct psmouse *psmouse, bool set_properties);
 int byd_init(struct psmouse *psmouse);
-#else
-static inline int byd_detect(struct psmouse *psmouse,
-				      bool set_properties)
-{
-	return -ENOSYS;
-}
-static inline int byd_init(struct psmouse *psmouse)
-{
-	return -ENOSYS;
-}
-
-#endif /* CONFIG_MOUSE_PS2_BYD */
 
 #endif /* !__BYD_H */
